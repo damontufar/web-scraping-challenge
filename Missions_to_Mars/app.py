830f4df58c75a76b5609145fa2b31e289184c393
Daniela Marroquín <daniela.montufar18@gmail.com>
2021-04-15 13:18:50 -0500

    Fixing scrape function

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -25,7 +25,7 @@ def home():
 @app.route("/scrape")
 def scraper():
      # Run the scrape function
-    mars_data = scrape_mars.scrape_info()
+    mars_data = scrape_mars.scrape()
 
     # Update the Mongo database using update and upsert=True
     mongo.db.collection.update({}, mars_data, upsert=True)
